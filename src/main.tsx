de648eff3001b371f2c449adca07b24886ed12ec
mirouwhite <amirdekik18@gmail.com>
2026-05-04 19:34:51 +0100

    feat: Integrate PostHog Analytics with Session Replays and Heatmaps

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
 import App from "./App.tsx";
 import "./index.css";
 import "./i18n";
+
+// Initialize PostHog Analytics
+posthog.init('phc_rVxbNnYaF5irWX2vYVNFHsTgCcjzauFEU8tkUP4rYZhD', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'always' // Required for advanced session replays
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
